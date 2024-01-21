64576eec065d34625c02a56aa9d333dade07b964
Jai Dalmotra <jaidalmotra14@gmail.com>
2024-01-21 20:23:25 +0530

    Create #31_Next_Permutation.cpp

---
+++ #31_Next_Permutation.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    void nextPermutation(vector<int>& nums) {
+        int n=nums.size();
+        int index1=-1;
+
+        for(int i=n-1;i>=1;i--)
+        {
+            if(nums[i]>nums[i-1])
+            {
+                index1=i-1;
+                break;
+            }
+        }
+        if(index1==-1)
+        {
+            reverse(nums.begin(),nums.end());
+        }
+        else
+        {
+            int index2=-1;
+            for(int i=n-1;i>=index1;i--)
+            {
+                if(nums[i]>nums[index1])
+                {
+                    index2=i;
+                    break;
+                }
+            }
+            swap(nums[index1],nums[index2]);
+            reverse(nums.begin()+index1+1,nums.end());
+        }
+    }
+};
